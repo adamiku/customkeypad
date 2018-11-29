6d38488959e402e0d26c7bf30170d88e0dd95837
Adam Mikulas <adam.mikulas@betssongroup.com>
2018-11-29 09:12:02 +0100

    fasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgVirtualKeyboardModule} from '@protacon/ng-virtual-keyboard';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgVirtualKeyboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
